26c322f2cbecc34682710dd8d76a3d444deca67d
Abin Babu <abinbabuonline@gmail.com>
2024-08-24 11:56:24 +0530

    REACTING

---
+++ react-app/src/Food.jsx
@@ -0,0 +1,14 @@
+function Food(){
+    const food1 = 'orange';
+    const food2 = 'banana'
+    
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>{food1.toLowerCase()}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    )
+}
+
+export default Food
