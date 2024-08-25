5fa1ba708c6a21e30a62027f66ded93670a4f0c8
Abin Babu <abinbabuonline@gmail.com>
2024-08-25 13:03:49 +0530

    listing items

---
+++ react-app/src/List.jsx
@@ -0,0 +1,35 @@
+
+// function List(){
+//     const fruits = [{id : 1, name: "apple" , calories: 95},
+//                     {id : 2,name: "orange" , calories: 45},
+//                     {id : 3,name: 'banana' , calories : 45 },
+//                     {id : 4, name : 'pineapple', calories : 76},
+//                     {id : 5,name : 'coconut' ,  calories : 37}]
+
+//     fruits.sort((a,b)  =>  a.name.localeCompare(b.name))
+    
+//     const listItems = fruits.map(fruit  =><li key={fruit.id}>{fruit.name} : &nbsp; <b>{fruit.calories}</b></li> )
+//     return (<ol>{listItems}</ol>)
+// }
+
+// export default List
+
+function List(props){
+
+    const category = props.category;
+    
+    const itemList = props.items;
+
+    //fruits.sort((a,b)  =>  a.name.localeCompare(b.name))
+    
+    const listItems = itemList.map(item  =><li key={item.id}>{item.name} : &nbsp; <b>{item.calories}</b></li> )
+    return (<>
+                <h3 className="list-category">{category}</h3>
+                <ol className="list-items">{listItems}</ol>
+            </>)
+}
+
+export default List
+
+
+
--- react-app/src/userGreeting.jsx
+++ react-app/src/userGreeting.jsx
@@ -1,9 +1,21 @@
 
-function userGreeting(props){
-    if(props.isLoggedIn){
-        return <h2>Welcome {props.username}</h2>
-    }
-    return <h2>Please log in to continue</h2>
+import propTypes from 'prop-types'
+
+function UserGreeting(props){
+    const welcomeMessage =  <h2 className="welcome-message">Welcome {props.username}</h2>
+    const loginPrompt = <h2 className="login-prompt">Please log in and continue</h2>
+    return (props.isLoggedIn ? welcomeMessage : loginPrompt)
+
 }
 
-export default userGreeting
+UserGreeting: propTypes = {
+    isLoggedIn: propTypes.bool,
+    username: propTypes.string
+}
+
+UserGreeting.defaultProps = {
+    isLoggedIn: false,
+    username: "Guest",
+}
+
+export default UserGreeting
