2bc33784dcb765222eb3a4f07026e5623fe3d875
Abin Babu <abinbabuonline@gmail.com>
2024-08-25 16:31:50 +0530

    button onClick

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -28,38 +28,48 @@
 
 
 
-import List from './List.jsx'
+// import List from './List.jsx'
 
-function App() {
+// function App() {
 
-  const fruits = [{id : 1, name: "apple" , calories: 95},
-                  {id : 2,name: "orange" , calories: 45},
-                  {id : 3,name: 'banana' , calories : 45 },
-                  {id : 4, name : 'pineapple', calories : 76},
-                  {id : 5,name : 'coconut' ,  calories : 37}]
+//   const fruits = [{id : 1, name: "apple" , calories: 95},
+//                   {id : 2,name: "orange" , calories: 45},
+//                   {id : 3,name: 'banana' , calories : 45 },
+//                   {id : 4, name : 'pineapple', calories : 76},
+//                   {id : 5,name : 'coconut' ,  calories : 37}]
 
-  const vegetables = [{id : 6, name: "potatoes" , calories: 110},
-                      {id : 7,name: "celery" , calories: 15},
-                      {id : 8,name: 'carrots' , calories :25 },
-                      {id : 9, name : 'corn', calories : 63},
-                      {id : 10,name : 'broccoli' ,  calories : 50}]
-  return(
+//   const vegetables = [{id : 6, name: "potatoes" , calories: 110},
+//                       {id : 7,name: "celery" , calories: 15},
+//                       {id : 8,name: 'carrots' , calories :25 },
+//                       {id : 9, name : 'corn', calories : 63},
+//                       {id : 10,name : 'broccoli' ,  calories : 50}]
+//   return(
 
-    <>
-            {fruits.length > 0 && <List category = 'Fruits'/> }
-            {vegetables.length > 0 && <List items = {vegetables} category = 'Vegetables'/>}
-    </>
+//     <>
+//             {fruits.length > 0 && <List category = 'Fruits'/> }
+//             {vegetables.length > 0 && <List items = {vegetables} category = 'Vegetables'/>}
+//     </>
   
-  );
+//   );
+// }
+
+
+
+// List.defaultProps = {
+//   category: "Category",
+//   items : []
+
+// }
+
+
+import Button from './Button'
+
+function App(){
+  return(<>
+  <Button/>
+  </>)
 }
 
-
-
-List.defaultProps = {
-  category: "Category",
-  items : []
-
-}
 
 export default App
 
--- react-app/src/Button.jsx
+++ react-app/src/Button.jsx
@@ -1,16 +1,31 @@
  
  //inline styling
 
- function Button(){
-    const styles = {
-            backgroundColor: "hsl(200, 100%, 50%)",
-            color: "white",
-            padding: "10px 20px",
-            borderRadius: "10px",
-            border: "none",
-            cursor: "pointer",
-           }
-    return (<button style={styles}>Click Me</button>)
+//  function Button(){
+//     const styles = {
+//             backgroundColor: "hsl(200, 100%, 50%)",
+//             color: "white",
+//             padding: "10px 20px",
+//             borderRadius: "10px",
+//             border: "none",
+//             cursor: "pointer",
+//            }
+//     return (<button style={styles}>Click Me</button>)
+// }
+
+let count = 0
+function Button(){
+    const handleClick = (name) => {
+        if(count < 3){
+            count++
+            console.log(`${name} you clicked ${count} times`);
+        }else{
+            console.log(`${name} stop clicking me `);
+            
+        }
+    }
+    
+    return (<button onClick={()=>handleClick("Bro")}>Click Me</button>)
 }
 
 export default Button
