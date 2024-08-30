a05f897012c2728accc6073b172dfb5800578b77
Abin Babu <abinbabuonline@gmail.com>
2024-08-30 18:51:32 +0530

    reacting

--- App.js
+++ App.js
@@ -1,4 +1,18 @@
-const heading = React.createElement("h1", {id:"heading"}, "Hello World")
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
-console.log(heading); //heading
+// const heading = React.createElement("h1", {id:"heading"}, "Hello World")
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading)
+// console.log(heading); //heading
+
+
+const parent = React.createElement("div", {id:"parent"} ,[
+                React.createElement("h2", {id:"child1"} ,"This is H2" ),
+                React.createElement("h1", {id:"child2"} ,"This is H1" )
+            ])
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+
+
+
