e8caea56ca8fa8b9715961f0bf3bda0045c4fc1f
Abin Babu <abinbabuonline@gmail.com>
2024-09-27 23:29:37 +0530

    react week completed

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,272 +1,300 @@
-// import Header from "./Header"
-// import Footer from "./footer"
-// import Food from "./Food"
-// import Card from "./Card"
-// import Button from "./Button"
-// import Student from "./Student"
+// // import Header from "./Header"
+// // import Footer from "./footer"
+// // import Food from "./Food"
+// // import Card from "./Card"
+// // import Button from "./Button"
+// // import Student from "./Student"
 
-//import { useState } from "react";
+// //import { useState } from "react";
 
-{
-  /* <Student name ="Abin Babu" age = {30} isStudent = {false}/>
-<Student name ="Patrick" age = {42} isStudent = {true}/>
-<Student name ="Fedrick" age = {23} isStudent = {true}/>
-<Student name ="Rozario" age = {32} isStudent = {false}/>
-<Student name ="Milan" age = {33} isStudent = {true}/>
-<Student name = "Arjun"/> */
-}
-
-import { Container } from 'postcss'
-// import UserGreeting from './UserGreeting.jsx'
-
-// function App() {
-//   return(
-//     <>
-//       <UserGreeting isLoggedIn = {false} username = "Abin"/>
-//     </>
-//   );
+// {
+//   /* <Student name ="Abin Babu" age = {30} isStudent = {false}/>
+// <Student name ="Patrick" age = {42} isStudent = {true}/>
+// <Student name ="Fedrick" age = {23} isStudent = {true}/>
+// <Student name ="Rozario" age = {32} isStudent = {false}/>
+// <Student name ="Milan" age = {33} isStudent = {true}/>
+// <Student name = "Arjun"/> */
 // }
 
-// export default App
+// import { Container } from 'postcss'
+// // import UserGreeting from './UserGreeting.jsx'
 
-// import List from './List.jsx'
+// // function App() {
+// //   return(
+// //     <>
+// //       <UserGreeting isLoggedIn = {false} username = "Abin"/>
+// //     </>
+// //   );
+// // }
 
-// function App() {
+// // export default App
 
-//   const fruits = [{id : 1, name: "apple" , calories: 95},
-//                   {id : 2,name: "orange" , calories: 45},
-//                   {id : 3,name: 'banana' , calories : 45 },
-//                   {id : 4, name : 'pineapple', calories : 76},
-//                   {id : 5,name : 'coconut' ,  calories : 37}]
+// // import List from './List.jsx'
 
-//   const vegetables = [{id : 6, name: "potatoes" , calories: 110},
-//                       {id : 7,name: "celery" , calories: 15},
-//                       {id : 8,name: 'carrots' , calories :25 },
-//                       {id : 9, name : 'corn', calories : 63},
-//                       {id : 10,name : 'broccoli' ,  calories : 50}]
-//   return(
+// // function App() {
 
-//     <>
-//             {fruits.length > 0 && <List category = 'Fruits'/> }
-//             {vegetables.length > 0 && <List items = {vegetables} category = 'Vegetables'/>}
-//     </>
+// //   const fruits = [{id : 1, name: "apple" , calories: 95},
+// //                   {id : 2,name: "orange" , calories: 45},
+// //                   {id : 3,name: 'banana' , calories : 45 },
+// //                   {id : 4, name : 'pineapple', calories : 76},
+// //                   {id : 5,name : 'coconut' ,  calories : 37}]
 
-//   );
-// }
+// //   const vegetables = [{id : 6, name: "potatoes" , calories: 110},
+// //                       {id : 7,name: "celery" , calories: 15},
+// //                       {id : 8,name: 'carrots' , calories :25 },
+// //                       {id : 9, name : 'corn', calories : 63},
+// //                       {id : 10,name : 'broccoli' ,  calories : 50}]
+// //   return(
 
-// List.defaultProps = {
-//   category: "Category",
-//   items : []
+// //     <>
+// //             {fruits.length > 0 && <List category = 'Fruits'/> }
+// //             {vegetables.length > 0 && <List items = {vegetables} category = 'Vegetables'/>}
+// //     </>
 
-// }
+// //   );
+// // }
 
-// import Button from './Button'
-// import ProfilePicture from './profilePicture'
+// // List.defaultProps = {
+// //   category: "Category",
+// //   items : []
 
-// function App(){
-//   return(<>
-//   <ProfilePicture/>
-//   </>)
-// }
+// // }
 
-// export default App
+// // import Button from './Button'
+// // import ProfilePicture from './profilePicture'
 
-// import MyComponent from "./myComponents"
+// // function App(){
+// //   return(<>
+// //   <ProfilePicture/>
+// //   </>)
+// // }
 
-// function App(){
-//     return(<MyComponent/>)
-// }
+// // export default App
 
-// export default App
+// // import MyComponent from "./myComponents"
 
-// import Counter from "./Counter"
+// // function App(){
+// //     return(<MyComponent/>)
+// // }
 
-// function  App() {
-//     return(<Counter/>)
-// }
+// // export default App
 
-// export default App
+// // import Counter from "./Counter"
 
-// import MyComponent from './myComponents'
+// // function  App() {
+// //     return(<Counter/>)
+// // }
 
-// function  App() {
-//     return(<MyComponent/>)
-// }
+// // export default App
 
-// export default App
+// // import MyComponent from './myComponents'
 
-// import ColorPicker from "./ColorPicker"
+// // function  App() {
+// //     return(<MyComponent/>)
+// // }
 
-// function  App() {
-//     return(<ColorPicker/>)
-// }
+// // export default App
 
-// export default App
+// // import ColorPicker from "./ColorPicker"
 
-// import DivShow from "./div";
-// import Header from "./HeaderName";
+// // function  App() {
+// //     return(<ColorPicker/>)
+// // }
 
-// function App(){
-//     const data = "Abin"
-//     return(
+// // export default App
 
-//                 <Header data={data}/>
+// // import DivShow from "./div";
+// // import Header from "./HeaderName";
 
-//         )
-// }
+// // function App(){
+// //     const data = "Abin"
+// //     return(
 
-// export default App
+// //                 <Header data={data}/>
 
-// import { useState } from "react"
+// //         )
+// // }
 
-// function App() {
+// // export default App
 
-//     const [count , setCount] = useState(0)
-//     const addButton = ()=>{
-//         setCount(count+1)
-//     }
-//     const subButton = ()=>{
-//         setCount(count-1)
-//     }
-//     const resetButton = ()=>{
-//         setCount(0)
-//     }
-//     return(
-//           <div className="counter">
-//             <h1>{count}</h1>
-//             <button onClick={addButton}>INCREMENT</button>
-//             <button onClick={resetButton}>RESET</button>
-//             <button onClick={subButton}>DECREMENT</button>
-//             </div>
+// // import { useState } from "react"
 
-//     )
+// // function App() {
 
-// }
+// //     const [count , setCount] = useState(0)
+// //     const addButton = ()=>{
+// //         setCount(count+1)
+// //     }
+// //     const subButton = ()=>{
+// //         setCount(count-1)
+// //     }
+// //     const resetButton = ()=>{
+// //         setCount(0)
+// //     }
+// //     return(
+// //           <div className="counter">
+// //             <h1>{count}</h1>
+// //             <button onClick={addButton}>INCREMENT</button>
+// //             <button onClick={resetButton}>RESET</button>
+// //             <button onClick={subButton}>DECREMENT</button>
+// //             </div>
 
-// export default App
+// //     )
 
-// import Counter from "./Counter"
+// // }
 
-// function App() {
-//   const [count, setCount] = useState(0);
-//   const [message , setMessage] = useState('')
-//   return (
-//     <div>
-//       <h1>{count}</h1>
-//       <button onClick={() => setCount(count + 1)}>INCREMENT</button>
-//       <button onClick={() => setCount(count - 1)}>DECREMENT</button>
-//     </div>
-//   );
-// }
+// // export default App
 
-// export default App;
+// // import Counter from "./Counter"
 
-// function App() {
-//   const [username , setUsername] = useState('')
-//   const [submit , setSubmit ] = useState(false)
-//   const handleSubmit = (e)=> {
-//     e.preventDefault()
-//     if(username.length < 6){
-//       setSubmit(true)
-//     }
-//   }
-//   return (
-//     <div style={{textAlign: 'center' , marginTop: '400px' , display: 'flex'}}>
-//       <form oncLick={handleSubmit}>
-//         <div>
-//           <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)}/>
-//           { username.length < 6 && <p style={{color: 'red'}}>username should have atleast 6 letters</p>}
-//           {submit && <p style={{color: 'green'}}>submitted</p>}
-//           <button style={{width:'60px' , height: '30px'}} disabled={username.length < 6}>submit</button>
-//         </div>
-//       </form>
-//     </div>
-//   )
-// }
+// // function App() {
+// //   const [count, setCount] = useState(0);
+// //   const [message , setMessage] = useState('')
+// //   return (
+// //     <div>
+// //       <h1>{count}</h1>
+// //       <button onClick={() => setCount(count + 1)}>INCREMENT</button>
+// //       <button onClick={() => setCount(count - 1)}>DECREMENT</button>
+// //     </div>
+// //   );
+// // }
 
-// export default App
+// // export default App;
 
-// import Dashboard from "./Toggle";
-//  import ParentComponent from "./ParentComponent";
+// // function App() {
+// //   const [username , setUsername] = useState('')
+// //   const [submit , setSubmit ] = useState(false)
+// //   const handleSubmit = (e)=> {
+// //     e.preventDefault()
+// //     if(username.length < 6){
+// //       setSubmit(true)
+// //     }
+// //   }
+// //   return (
+// //     <div style={{textAlign: 'center' , marginTop: '400px' , display: 'flex'}}>
+// //       <form oncLick={handleSubmit}>
+// //         <div>
+// //           <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)}/>
+// //           { username.length < 6 && <p style={{color: 'red'}}>username should have atleast 6 letters</p>}
+// //           {submit && <p style={{color: 'green'}}>submitted</p>}
+// //           <button style={{width:'60px' , height: '30px'}} disabled={username.length < 6}>submit</button>
+// //         </div>
+// //       </form>
+// //     </div>
+// //   )
+// // }
 
-// function App () {
+// // export default App
 
-//     return(
-//       <>
-//          <ParentComponent/>
+// // import Dashboard from "./Toggle";
+// //  import ParentComponent from "./ParentComponent";
+
+// // function App () {
+
+// //     return(
+// //       <>
+// //          <ParentComponent/>
             
-//       </>
+// //       </>
 
-//     )
-// }
+// //     )
+// // }
 
-// export default App
-
-
-
-// import React, { useCallback, useState } from 'react'
-// import IncrementButton from './IncrementButton'
-
-// const App = () => {
-//   const [count , setCount] = useState(0)
-//   const increment = useCallback(()=>{
-//     setCount((prevCount)=> prevCount+1)
-//   },[])
-//   return (
-//     <div>
-//       <h3>{count}</h3>
-//       <IncrementButton increment={increment}/>
-//     </div>
-//   )
-// }
-
-// export default App
-
-// import React, { useMemo, useState } from 'react'
-
-// const App = () => {
-//   const [array, setArray] = useState([1,2,3,4,5,6])
-//   const memoized = useMemo(()=> {
-//     return array.reduce((acc,curr)=> acc+curr)
-//   },[array])
-//   return (
-//     <div>
-//         <h2>{memoized}</h2>
-//         <button onClick={()=> setArray([2,4,5,3,8])}>Change Array</button>
-//     </div>
-//   )
-// }
-
-// export default App
+// // export default App
 
 
 
-// App.js
+// // import React, { useCallback, useState } from 'react'
+// // import IncrementButton from './IncrementButton'
+
+// // const App = () => {
+// //   const [count , setCount] = useState(0)
+// //   const increment = useCallback(()=>{
+// //     setCount((prevCount)=> prevCount+1)
+// //   },[])
+// //   return (
+// //     <div>
+// //       <h3>{count}</h3>
+// //       <IncrementButton increment={increment}/>
+// //     </div>
+// //   )
+// // }
+
+// // export default App
+
+// // import React, { useMemo, useState } from 'react'
+
+// // const App = () => {
+// //   const [array, setArray] = useState([1,2,3,4,5,6])
+// //   const memoized = useMemo(()=> {
+// //     return array.reduce((acc,curr)=> acc+curr)
+// //   },[array])
+// //   return (
+// //     <div>
+// //         <h2>{memoized}</h2>
+// //         <button onClick={()=> setArray([2,4,5,3,8])}>Change Array</button>
+// //     </div>
+// //   )
+// // }
+
+// // export default App
+
+
+
+// // App.js
+
+// // import React from 'react'
+// // import { ParentProvider } from './ParentProvider'
+
+// // const App = () => {
+// //   return (
+// //     <div>
+// //         <ParentProvider />
+// //     </div>
+// //   )
+// // }
+
+// // export default App
+
+// // import React from 'react'
+// // import Parent from './Parent'
+
+// // const App = () => {
+// //   return (
+// //     <div>
+// //       <Parent/>
+// //     </div>
+// //   )
+// // }
+
+// // export default App
 
 // import React from 'react'
-// import { ParentProvider } from './ParentProvider'
+// import RedCounter from './RedCounter'
 
 // const App = () => {
 //   return (
 //     <div>
-//         <ParentProvider />
+//       <RedCounter />
 //     </div>
 //   )
 // }
 
 // export default App
 
+
+
+
+
 import React from 'react'
-import Parent from './Parent'
+import ReduceCounter from './ReduceCounter'
 
 const App = () => {
   return (
     <div>
-      <Parent/>
+      <ReduceCounter/>
     </div>
   )
 }
 
 export default App
-
-
--- react-app/src/RedCounter.jsx
+++ react-app/src/RedCounter.jsx
@@ -1,27 +1,26 @@
-import React, { useReducer } from "react";
+import React, { useReducer } from 'react'
 
-const initialState = { count: 0 };
+const initialState = {count : 0}
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "increment":
-      return { count: state.count + 1 };
-    case "decrement":
-      return { count: state.count - 1 };
-    case "reset":
-      return { count: 0 };
-  }
-};
+const reducer = (state , action)=> {
+    switch(action.type) {
+        case 'INCREMENT' : return {count : state.count + 1}
+        case 'DECREMENT' : return {count : state.count - 1}
+        case 'RESET' : return {count : 0}
+    }
+}
 
 const RedCounter = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
 
-  return (<div>
-    <h1>{state.count}</h1>
-    <button onClick={()=> dispatch({type: 'increment'})}>INCREMENT</button>
-    <button onClick={()=> dispatch({type: 'reset'})}>RESET</button>
-    <button onClick={()=> dispatch({type: 'decrement'})}>DECREMENT</button>
-  </div>);
-};
+  const [state , dispatch ] = useReducer( reducer , initialState )
+  return (
+    <div>
+      <h1>{state.count}</h1>
+      <button onClick={()=> {dispatch({type : 'INCREMENT'})}}>INCREMENT</button>
+      <button onClick={()=> {dispatch({type : 'DECREMENT'})}}>DECREMENT</button>
+      <button onClick={()=> {dispatch({type : 'RESET'})}}>RESET</button>
+    </div>
+  )
+}
 
-export default RedCounter;
+export default RedCounter
